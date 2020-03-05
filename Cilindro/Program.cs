ae647ab2168eb3eddd0314d64eee63ffad919d7c
NelsonSalvador <nelsonsalvadorzx@gmail.com>
2020-03-05 15:58:53 +0000

    Volume e area do cilindro

---
+++ Cilindro/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Cilindro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a_s, rs;
+            float v, s, a, r;
+            const float pi = 3.1415926f;
+
+            Console.Write("Altura: ");
+            a_s = Console.ReadLine();
+
+            Console.Write("Raio: ");
+            rs = Console.ReadLine();
+
+            a = float.Parse(a_s);
+            r = float.Parse(rs);
+
+            v = pi * r * r * a;
+            s = 2*pi*r*(r+a);
+            Console.WriteLine($"Volume = {v,8:f2}");
+            Console.WriteLine($"Area = {s,8:f2}");
+        }
+    }
+}
