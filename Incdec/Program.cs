0d816e35b96b9c46bf8edccd63d77e59a0e03b07
NelsonSalvador <nelsonsalvadorzx@gmail.com>
2020-03-05 14:38:02 +0000

    increment

---
+++ Incdec/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Incdec
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 2;
+            a = (a++ + a++);
+            Console.WriteLine(a);
+
+        }
+    }
+}
