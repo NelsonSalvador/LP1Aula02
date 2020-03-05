cb297e116106945694815cf125054d0b69b093d7
NelsonSalvador <nelsonsalvadorzx@gmail.com>
2020-03-05 17:09:16 +0000

    Overflow e underflow

--- ValoresEspeciais/Program.cs
+++ ValoresEspeciais/Program.cs
@@ -12,6 +12,16 @@ namespace ValoresEspeciais
             Console.WriteLine(sbyte.MaxValue);
             Console.WriteLine(double.MinValue);
             Console.WriteLine(double.MaxValue);
+
+            uint j = uint.MaxValue;
+            int i = short.MaxValue;
+            float f1, f2;
+
+            Console.WriteLine((uint)(j + 1));
+            Console.WriteLine((short)(i+1));
+            f1 = f2 = 10000.0f;
+            Console.WriteLine(f1 == f2 + 0.0004f);
+              
         }
     }
 }
