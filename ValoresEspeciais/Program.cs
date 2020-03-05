108244fe129c4001df6bee88d5322ad198a038b9
NelsonSalvador <nelsonsalvadorzx@gmail.com>
2020-03-05 16:48:38 +0000

    Print de Valores especiasi

---
+++ ValoresEspeciais/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ValoresEspeciais
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(uint.MaxValue);
+            Console.WriteLine(double.PositiveInfinity);
+            Console.WriteLine(double.NegativeInfinity);
+            Console.WriteLine(sbyte.MaxValue);
+            Console.WriteLine(double.MinValue);
+            Console.WriteLine(double.MaxValue);
+        }
+    }
+}
